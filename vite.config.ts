825e62396b7f42d0bb982fd323c27093252caf55
Hans <votuanhung12052006@gmail.com>
2025-11-13 02:32:39 +0700

    fix: remove base config for Render deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-const repo = 'Pu'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/${repo}/`,
 })
