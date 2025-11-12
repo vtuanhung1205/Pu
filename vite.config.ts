d0c7f0cb77640d000d7ddd028ae8bb4c09e8fc4e
Hans <votuanhung12052006@gmail.com>
2025-11-13 02:20:03 +0700

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const repo = 'Pu'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `/${repo}/`,
 })
